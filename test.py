6a4489082848c3505b82bf695828e93d816f5449
DegtiarevDA <DegtjarjowDA@yandex.ru>
2021-09-11 14:51:51 +0300

    add 1/3 lab1

--- test.py
+++ test.py
@@ -1,1 +1,90 @@
-print("Hello, my team!")
+import math as m
+import main
+
+from math import sqrt as s
+
+main.print_hi()
+m.pi
+s()
+#
+# # объявил переменную и присвоил ей значение
+# value = 10.1
+#
+# studentName = 'Alex'
+#
+# age = 21
+#
+# sum1 = age > value
+#
+# age2 = float(age)
+#
+# # список
+# s = list()
+# s1 = ["M" , "E"]
+# s1.append("I")
+# s2 = s1[2]
+#
+# s3 = tuple()
+# s3 = (1, 2)
+# f = s3[1]
+#
+#
+# d = dict()
+# d = {1 : 'Vasya'}
+# d = {2 : 'Semen'}
+#
+# a = d.get(2)
+#
+# # множество
+# s = set()
+#
+# s.add(1)
+# s.add(2)
+# s.add(2)
+#
+# sss = False
+#
+# if value > age:
+#     print("i'm here!")
+#
+# print('fdsg')
+#
+# if True:
+#     print('fdsf')
+# elif sss:
+#     print(sss)
+# else:
+#     print('Не прошли проверки')
+#
+# tttt = True
+# i = 0
+# # while tttt:
+#     print("aaaaa")
+#     i += 1
+#     if i > 10:
+#         tttt = False
+
+
+# for i in s1:
+#     print(i)
+
+
+
+
+
+# for letter in 'лучшийggggggt':
+#     if letter == 'g':
+#         continue
+#     elif letter == 'a':
+#         break
+#     print(letter)
+
+
+
+
+# str2 = 'Я люблю МЭИ'
+#
+# str1 = 'по субботам'
+#
+#
+# print(str2 + ' ' + str1 + ' ' + str(value) + ' раз')
